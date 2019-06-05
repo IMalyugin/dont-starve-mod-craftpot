632479b7e08fd2f266e96f748b02cfbef77c30aa
IMalyugin <ivanxru@mail.ru>
2019-06-06 01:33:37 +0300

    [0.9.7] Rename AddFoodTags to AddFoodTag in public API

--- scripts/ingredienttags.lua
+++ scripts/ingredienttags.lua
@@ -8,7 +8,7 @@ FOODTAGDEFINITIONS = FOODTAGDEFINITIONS or {}
 -- Function performs merge, so it is safe to call multiple times or with different data
 --
 -- So Far TagData only takes following keys: atlas, tex, name
-local AddFoodTags = function(tag, data)
+local AddFoodTag = function(tag, data)
   local mergedData = FOODTAGDEFINITIONS[tag] or {}
 
   for k, v in pairs(data) do
@@ -19,56 +19,56 @@ local AddFoodTags = function(tag, data)
 end
 
 -- common tags
-AddFoodTags('meat', { name= 'Meats', atlas="images/food_tags.xml" })
-AddFoodTags('veggie', { name="Vegetables", atlas="images/food_tags.xml" })
-AddFoodTags('fish', { name="Fish", atlas="images/food_tags.xml" })
-AddFoodTags('sweetener', { name="Sweets", atlas="images/food_tags.xml" })
+AddFoodTag('meat', { name= 'Meats', atlas="images/food_tags.xml" })
+AddFoodTag('veggie', { name="Vegetables", atlas="images/food_tags.xml" })
+AddFoodTag('fish', { name="Fish", atlas="images/food_tags.xml" })
+AddFoodTag('sweetener', { name="Sweets", atlas="images/food_tags.xml" })
 
-AddFoodTags('monster', { name="Monster Foods", atlas="images/food_tags.xml" })
-AddFoodTags('fruit', { name="Fruits", atlas="images/food_tags.xml" })
-AddFoodTags('egg', { name="Eggs", atlas="images/food_tags.xml" })
-AddFoodTags('inedible', { name="Inedibles", atlas="images/food_tags.xml" })
+AddFoodTag('monster', { name="Monster Foods", atlas="images/food_tags.xml" })
+AddFoodTag('fruit', { name="Fruits", atlas="images/food_tags.xml" })
+AddFoodTag('egg', { name="Eggs", atlas="images/food_tags.xml" })
+AddFoodTag('inedible', { name="Inedibles", atlas="images/food_tags.xml" })
 
-AddFoodTags('frozen', { name="Ice", atlas="images/food_tags.xml" })
-AddFoodTags('magic', { name="Magic", atlas="images/food_tags.xml" })
-AddFoodTags('decoration', { name="Decoration", atlas="images/food_tags.xml" })
-AddFoodTags('seed', { name="Seeds", atlas="images/food_tags.xml" })
+AddFoodTag('frozen', { name="Ice", atlas="images/food_tags.xml" })
+AddFoodTag('magic', { name="Magic", atlas="images/food_tags.xml" })
+AddFoodTag('decoration', { name="Decoration", atlas="images/food_tags.xml" })
+AddFoodTag('seed', { name="Seeds", atlas="images/food_tags.xml" })
 
-AddFoodTags('dairy', { name="Dairies", atlas="images/food_tags.xml" })
-AddFoodTags('fat', { name="Fat", atlas="images/food_tags.xml" })
+AddFoodTag('dairy', { name="Dairies", atlas="images/food_tags.xml" })
+AddFoodTag('fat', { name="Fat", atlas="images/food_tags.xml" })
 
-AddFoodTags('alkaline', { name="Alkaline", atlas="images/food_tags.xml" })
-AddFoodTags('flora', { name="Flora", atlas="images/food_tags.xml" })
-AddFoodTags('fungus', { name="Fungi", atlas="images/food_tags.xml" })
-AddFoodTags('leek', { name="Leek", atlas="images/food_tags.xml" })
-AddFoodTags('citrus', { name="Citrus", atlas="images/food_tags.xml" })
+AddFoodTag('alkaline', { name="Alkaline", atlas="images/food_tags.xml" })
+AddFoodTag('flora', { name="Flora", atlas="images/food_tags.xml" })
+AddFoodTag('fungus', { name="Fungi", atlas="images/food_tags.xml" })
+AddFoodTag('leek', { name="Leek", atlas="images/food_tags.xml" })
+AddFoodTag('citrus', { name="Citrus", atlas="images/food_tags.xml" })
 
-AddFoodTags('dairy_alt', { name="Dairy", atlas="images/food_tags.xml" })
-AddFoodTags('fat_alt', { name="Fat", atlas="images/food_tags.xml" })
+AddFoodTag('dairy_alt', { name="Dairy", atlas="images/food_tags.xml" })
+AddFoodTag('fat_alt', { name="Fat", atlas="images/food_tags.xml" })
 
-AddFoodTags('mushrooms', { name="Mushrooms", atlas="images/food_tags.xml" })
-AddFoodTags('nut', { name="Nuts", atlas="images/food_tags.xml" })
-AddFoodTags('poultry', { name="Poultries", atlas="images/food_tags.xml" })
-AddFoodTags('pungent', { name="Pungents", atlas="images/food_tags.xml" })
-AddFoodTags('grapes', { name="Grapes", atlas="images/food_tags.xml" })
+AddFoodTag('mushrooms', { name="Mushrooms", atlas="images/food_tags.xml" })
+AddFoodTag('nut', { name="Nuts", atlas="images/food_tags.xml" })
+AddFoodTag('poultry', { name="Poultries", atlas="images/food_tags.xml" })
+AddFoodTag('pungent', { name="Pungents", atlas="images/food_tags.xml" })
+AddFoodTag('grapes', { name="Grapes", atlas="images/food_tags.xml" })
 
-AddFoodTags('decoration_alt', { name="Decoration", atlas="images/food_tags.xml" })
-AddFoodTags('seed_alt', { name="Seeds", atlas="images/food_tags.xml" })
+AddFoodTag('decoration_alt', { name="Decoration", atlas="images/food_tags.xml" })
+AddFoodTag('seed_alt', { name="Seeds", atlas="images/food_tags.xml" })
 
-AddFoodTags('root', { name="Roots", atlas="images/food_tags.xml" })
-AddFoodTags('seafood', { name="Seafood", atlas="images/food_tags.xml" })
-AddFoodTags('shellfish', { name="Shellfish", atlas="images/food_tags.xml" })
-AddFoodTags('spices', { name="Spices", atlas="images/food_tags.xml" })
-AddFoodTags('wings', { name="Wings", atlas="images/food_tags.xml" })
+AddFoodTag('root', { name="Roots", atlas="images/food_tags.xml" })
+AddFoodTag('seafood', { name="Seafood", atlas="images/food_tags.xml" })
+AddFoodTag('shellfish', { name="Shellfish", atlas="images/food_tags.xml" })
+AddFoodTag('spices', { name="Spices", atlas="images/food_tags.xml" })
+AddFoodTag('wings', { name="Wings", atlas="images/food_tags.xml" })
 
-AddFoodTags('monster_alt', { name="Monster Foods", atlas="images/food_tags.xml" })
-AddFoodTags('sweetener_alt', { name="Sweets", atlas="images/food_tags.xml" })
+AddFoodTag('monster_alt', { name="Monster Foods", atlas="images/food_tags.xml" })
+AddFoodTag('sweetener_alt', { name="Sweets", atlas="images/food_tags.xml" })
 
-AddFoodTags('squash', { name="Squash", atlas="images/food_tags.xml" })
-AddFoodTags('starch', { name="Starch", atlas="images/food_tags.xml" })
-AddFoodTags('tuber', { name="Tuber", atlas="images/food_tags.xml" })
-AddFoodTags('precook', { name="Precooked", atlas="images/food_tags.xml" })
-AddFoodTags('cactus', { name="Cactus", atlas="images/food_tags.xml" })
+AddFoodTag('squash', { name="Squash", atlas="images/food_tags.xml" })
+AddFoodTag('starch', { name="Starch", atlas="images/food_tags.xml" })
+AddFoodTag('tuber', { name="Tuber", atlas="images/food_tags.xml" })
+AddFoodTag('precook', { name="Precooked", atlas="images/food_tags.xml" })
+AddFoodTag('cactus', { name="Cactus", atlas="images/food_tags.xml" })
 
-global("AddFoodTags")
-AddFoodTags = AddFoodTags
+global("AddFoodTag")
+AddFoodTag = AddFoodTag
